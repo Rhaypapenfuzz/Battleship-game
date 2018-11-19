af16715ffd79d9c107f59869f3c1adc7075574e6
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2018-11-18 23:44:13 -0500

    Update source.cpp
    implementation of get_board_data function

--- source.cpp
+++ source.cpp
@@ -9,3 +9,25 @@ void fire(gameBoard, int&, bool&);
 void print_board(gameBoard);
 bool winner_test(int, int);
 void gameintro();
+
+int main()
+{
+return 0;
+}
+void get_board_data(ifstream& infile, gameBoard gameBoard1, gameBoard gameBoard2)
+{
+	infile.open("infile.txt");
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++) {
+			infile >> gameBoard1[i][j];
+		}
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++) {
+			infile >> gameBoard2[i][j];
+		}
+	}
+  infile.close();
+}
